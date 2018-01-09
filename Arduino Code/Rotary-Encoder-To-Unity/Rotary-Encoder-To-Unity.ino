af6e10a05bbe5d39641cc8e369c8e91f286427aa
T-Kuhn <t-kuhn@infiniteloop.co.jp>
2018-01-09 18:25:48 +0900

    Added new code for a rotary encoder setup.

---
+++ Arduino Code/Rotary-Encoder-To-Unity/Rotary-Encoder-To-Unity.ino
@@ -0,0 +1,83 @@
+#include "Encoder.h"
+
+// 8 and 9 are inputs
+Encoder Encoder1(8, 9, 10, 11);
+
+void setup() {
+  Serial.begin(115200);
+
+  // - - - - - - - - - - - - - - - - - - -
+  // - - - - -  SET UP TIMER 1 - - - - - -
+  // - - - - - - - - - - - - - - - - - - - 
+  noInterrupts();               // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  OCR1A = 3;                   // compare match register 16MHz/256/20kHz
+  TCCR1B |= (1 << WGM12);      // CTC mode
+  TCCR1B |= (1 << CS12);       // 256 prescaler 
+  TIMSK1 |= (1 << OCIE1A);     // enable timer compare interrupt
+
+  interrupts();                // enable all interrupts
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - - TIMER 1 INTERRUPT FUNCTION  - - -
+// - - - - - every 60 us - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - 
+ISR(TIMER1_COMPA_vect)
+{
+  // ENCODER READ FUNCTION COMES HERE!
+  Encoder1.update();
+  digitalWrite(7, digitalRead(7) ^ 1);   // toggle pin 7
+}
+
+void loop() {
+  serialPrintf("val:%d", Encoder1.count);
+      
+  //send all 20ms. Thus, 50 times per second.
+  delay(20);
+}
+
+// Printing Values to the Serial port.
+// serialPrintf("someInt:%d,someFloat:%f,someString:%s", someInt, someFloat, someString);
+int serialPrintf(char *str, ...) {
+    int i, j, count = 0;
+
+    va_list argv;
+    va_start(argv, str);
+    for(i = 0, j = 0; str[i] != '\0'; i++) {
+        if (str[i] == '%') {
+            count++;
+
+            Serial.write(reinterpret_cast<const uint8_t*>(str+j), i-j);
+
+            switch (str[++i]) {
+                case 'd': Serial.print(va_arg(argv, int));
+                    break;
+                case 'l': Serial.print(va_arg(argv, long));
+                    break;
+                case 'f': Serial.print(va_arg(argv, double));
+                    break;
+                case 'c': Serial.print((char) va_arg(argv, int));
+                    break;
+                case 's': Serial.print(va_arg(argv, char *));
+                    break;
+                case '%': Serial.print("%");
+                    break;
+                default:;
+            };
+
+            j = i+1;
+        }
+    };
+    va_end(argv);
+
+    if(i > j) {
+        Serial.write(reinterpret_cast<const uint8_t*>(str+j), i-j);
+    }
+
+    Serial.println("");
+    return count;
+}
---
+++ Unity Code/Arduino-Unity-Bridge/Assets/Scripts/RotaryDataFromArduino.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using UnityEngine;
+
+public class RotaryDataFromArduino : MonoBehaviour
+{
+    public GameObject go;
+    ArduinoSerial arduinoSerial;
+    Quaternion startRot;
+
+    void Start()
+    {
+        arduinoSerial = new ArduinoSerial();
+        arduinoSerial.ConnectToArduino(baudRate: 115200);
+        startRot = go.transform.rotation;
+    }
+
+    void Update()
+    {
+        if (arduinoSerial.ReadInput("val") == null)
+        {
+            return;
+        }
+
+        float value = float.Parse(arduinoSerial.ReadInput("val"), CultureInfo.InvariantCulture.NumberFormat);
+        Debug.Log("val: " + value);
+
+        go.transform.rotation = Quaternion.RotateTowards(go.transform.rotation, 
+           Quaternion.Euler( startRot.eulerAngles.x, startRot.eulerAngles.y, value / 600f * 360f ), Time.deltaTime * 500);
+    }
+
+    void OnApplicationQuit()
+    {
+        arduinoSerial.ClosePort();            
+    }
+}
